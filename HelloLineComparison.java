b1e829b6420ea5c9d3147eaa86820678c3233d69
unknown <gayoyawalkar@gmail.com>
2021-10-01 14:15:22 +0530

    Welcome to Line Comparison Program using class methods

---
+++ HelloLineComparison.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.linecomparison;
+public class HelloLineComparison {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Program");
+	}
+}
